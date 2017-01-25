019edcd6aa3f7714b56c445b23c71f376d7e287b
tongmbj <tongchen105@gmail.com>
2017-01-25 13:24:39 -0600

    Add files via upload

---
+++ audioInputStream.java
@@ -0,0 +1,26 @@
+package dap;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class audioInputStream {
+	public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+		// TODO Auto-generated method stub
+		AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(
+				  new File("mariachi.wav"));
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			FloatControl gainControl = 
+				  (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+			gainControl.setValue(-10.0f); // Reduce volume by 10 decibels.
+			clip.start();
+	}
+
+}
