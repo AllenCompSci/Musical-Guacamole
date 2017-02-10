1f2463c82c6161e874eb4b0187f1bbe72195ed42
cranidos24 <johnmatthew.bandelaria@student.allenisd.org>
2017-02-10 13:35:55 -0600

    Add files via upload

---
+++ Screen.java
@@ -0,0 +1,175 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Screen implements Runnable, KeyListener, WindowListener, MouseListener {
+	 	public final String TITLE = "Musical Guac";
+	    public final Dimension SIZE = new Dimension(1920, 1080); //SETS WINDOW DIMENSION
+	    public JFrame window;
+	    private int fontSize=300;
+	    private Rectangle choice1;
+	    private Rectangle choice2;
+	    private Rectangle choice3;
+	    private Rectangle tong;
+	    private Image imgbuffer;
+	    private boolean change;
+	    private boolean isrunner, isdoner;
+	
+	    
+	    public void setChange(boolean change) {
+	        this.change = change;
+	    }
+	    
+	    
+	 public Screen(){
+		 choice1= new Rectangle(400,600,1000,100);
+	     choice2=new Rectangle(400,700,1000,100);
+	     choice3=new Rectangle(400,800,1000,100);
+		 window=new JFrame();
+		 window.setSize(SIZE);
+	     window.setTitle(TITLE);
+	     window.setVisible(true);
+	     window.setLayout(null);
+	     isrunner=true;
+	     isdoner=false;
+	     setChange(true);
+	     imgbuffer = window.createImage(SIZE.width, SIZE.height);
+	 }
+	    
+	    public void run() {
+	        while(isrunner){
+		         vicks();
+		        
+	            if(change){
+	                setChange(false);
+
+	            }
+	            try{Thread.sleep(10000);}
+	            catch(InterruptedException ie){
+	                ie.printStackTrace();
+	            }
+	        }
+	        isdoner = true;
+	    }
+private void vicks(){
+    Graphics2D g2d = (Graphics2D) imgbuffer.getGraphics();
+    g2d.setColor(Color.black);
+    g2d.fillRect(0, 0, SIZE.width, SIZE.height); //actually fills screen
+    g2d.setFont(new Font("chiller",Font.BOLD, fontSize));
+    g2d.setColor(Color.green);
+    g2d.draw(choice1);
+    g2d.draw(choice2);
+    g2d.draw(choice3);
+    g2d.drawString("MUSICAL", 300, 300);
+    g2d.drawString("GUACAMOLE", 300,500);
+    g2d.setFont(new Font("chiller",Font.BOLD, 100));
+    g2d.drawString("PLAY", 800, 890);
+    g2d.drawString("CREDITS",740, 690);
+    g2d.drawString("OPTIONS",740,790);
+    g2d = (Graphics2D) window.getGraphics();
+    if(isrunner) 
+    g2d = (Graphics2D) window.getGraphics();
+    g2d.drawImage(imgbuffer, 0,  0, SIZE.width, SIZE.height, 0, 0, SIZE.width, SIZE.height, null);
+    if(hitbox whatever){
+    	g2d.setFont(new Font("chiller", Font.BOLD, fontSize));
+    	g2d.drawString("ALEX YU DAB", 300, 300);
+    }
+    g2d.dispose();
+    
+}
+
+@Override
+public void mouseClicked(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mousePressed(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseReleased(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseEntered(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseExited(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void windowOpened(WindowEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void windowClosing(WindowEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void windowClosed(WindowEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void windowIconified(WindowEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void windowDeiconified(WindowEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void windowActivated(WindowEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void windowDeactivated(WindowEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+}
