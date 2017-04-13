2607d550a4b4eb63b64343b1e61a10b143a8dd24
tongmbj <tongchen105@gmail.com>
2017-04-13 14:19:46 -0500

    Add files via upload
    enemy class for guacamole

---
+++ guac.java
@@ -0,0 +1,26 @@
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+
+public class guac {
+	private Rectangle hitbox;
+	private BufferedImage guacamoleImg;
+	public guac(int x, int y){
+		try {
+			guacamoleImg = ImageIO.read(this.getClass().getResource("guacamole2new.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		hitbox = new Rectangle(x, y, 100, 100);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
